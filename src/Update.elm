3ded34a782f40bd0bed13049b1d0162f9259b39f
CarlosIPe <carlos2@compendium.com.ar>
2017-11-29 04:17:08 -0500

    reorganize code and list of players

---
+++ src/Update.elm
@@ -0,0 +1,9 @@
+module Update exposing (update)
+
+import Models exposing (Model)
+import Msgs exposing (Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
